811a71c904255ce18f8879d3a0e2c821d4050959
FFFirer <0sdf0@sina.cn>
2020-05-29 01:05:19 +0800

    添加保存数据模块

---
+++ NetSpider.XieCheng/DB/CtripDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore;
+using NetSpider.XieCheng.DB.Entities;
+
+namespace NetSpider.XieCheng.DB
+{
+    public class CtripDbContext : DbContext
+    {
+        public CtripDbContext(DbContextOptions<CtripDbContext> options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Airline>().ToTable("airlines");
+            modelBuilder.Entity<Airport>().ToTable("airports");
+            modelBuilder.Entity<Cabin>().ToTable("cabins");
+            modelBuilder.Entity<Characteristic>().ToTable("characteristics");
+            modelBuilder.Entity<Flight>().ToTable("flights");
+        }
+
+        public DbSet<Airline> Airlines { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Cabin> Cabins { get; set; }
+        public DbSet<Characteristic> Characteristics { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
---
+++ NetSpider.XieCheng/DB/Entities/Characteristic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetSpider.XieCheng.DB.Entities
+{
+    /// <summary>
+    /// 特征描述
+    /// </summary>
+    public class Characteristic
+    {
+        public int Id { get; set; }
+        public int LinkedFlightId { get; set; }
+        public string FlightId { get; set; }
+        public string LowestPrice { get; set; }
+        public string LowestPriceId { get; set; }
+        public string LowestCfPrice { get; set; }
+        public string LowestChildPrice { get; set; }
+        public string LowestChildCfPrice { get; set; }
+        public string LowestChildAdultPrice { get; set; }
+        public string LowestChildAdultCfPrice { get; set; }
+    }
+}
