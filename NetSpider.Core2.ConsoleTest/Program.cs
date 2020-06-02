597c8542d7d08c1965b6bbb8bcd7fd695d208d74
Watt Wang <watt.wang@softtek.com>
2020-06-02 17:01:45 +0800

    准备重写框架

---
+++ NetSpider.Core2.ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetSpider.Core2.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
