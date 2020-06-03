f1fae3cf1d64e83af04d3725e8f06ada57385075
Watt Wang <watt.wang@softtek.com>
2020-06-03 17:47:17 +0800

    ClearScript Test success，puppeteeSharp test fail

---
+++ PuppeteerSharp.Example/ClearScriptDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.ClearScript.JavaScript;
+using Microsoft.ClearScript.V8;
+
+namespace PuppeteerSharp.Example
+{
+    public class ClearScriptDemo
+    {
+        public void Execute()
+        {
+            string ScriptFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts", "demo.js");
+            string scriptContent = string.Empty;
+            using(FileStream fs = new FileStream(ScriptFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                    scriptContent = sr.ReadToEnd().Replace("\r\n", "");
+                }
+            }
+
+            using (var engine = new V8ScriptEngine())
+            {
+                engine.DocumentSettings.AccessFlags = Microsoft.ClearScript.DocumentAccessFlags.EnableFileLoading;
+                engine.DefaultAccess = Microsoft.ClearScript.ScriptAccess.Full;
+                engine.CompileDocument(ScriptFilePath);
+                //var result = engine.Invoke("encodeURIComponent", "SHAURCOnewayduew&^%5d54nc'KH"); //只能调用全局方法
+                scriptContent += "m(\"SHAURCOnewayduew&^%5d54nc'KH\");";
+                var result = engine.Evaluate(scriptContent);
+
+            }
+        }
+    }
+}
---
+++ PuppeteerSharp.Example/Demo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PuppeteerSharp;
+using System.IO;
+
+namespace PuppeteerSharp.Example
+{
+    public class Demo
+    {
+        public async void Execute()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("https://flights.ctrip.com/itinerary/oneway/SHA-URC?date=2020-10-01");
+            var result = await page.GetContentAsync();
+            using (FileStream fs = new FileStream("output.html", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using(StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.Write(result);
+                    sw.Flush();
+                }
+            }
+        }
+    }
+}
