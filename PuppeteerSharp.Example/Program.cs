02d3a974fd3d58a170bc25f8b12fdf26a4b728bd
Watt Wang <watt.wang@softtek.com>
2020-06-03 17:47:17 +0800

    ClearScript Test success，puppeteeSharp test fail

---
+++ PuppeteerSharp.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PuppeteerSharp.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ClearScriptDemo demo = new ClearScriptDemo();
+            demo.Execute();
+
+
+            Console.ReadKey();
+        }
+    }
+}
