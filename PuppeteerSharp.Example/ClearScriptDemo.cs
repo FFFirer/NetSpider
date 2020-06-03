bf22c57e42ddc06cc39f847adcc49a37f896b849
FFFirer <0sdf0@sina.cn>
2020-06-03 22:02:32 +0800

    如何调用js文件中的方法解决

--- PuppeteerSharp.Example/ClearScriptDemo.cs
+++ PuppeteerSharp.Example/ClearScriptDemo.cs
@@ -25,10 +25,15 @@ namespace PuppeteerSharp.Example
             {
                 engine.DocumentSettings.AccessFlags = Microsoft.ClearScript.DocumentAccessFlags.EnableFileLoading;
                 engine.DefaultAccess = Microsoft.ClearScript.ScriptAccess.Full;
-                engine.CompileDocument(ScriptFilePath);
+
                 //var result = engine.Invoke("encodeURIComponent", "SHAURCOnewayduew&^%5d54nc'KH"); //只能调用全局方法
-                scriptContent += "m(\"SHAURCOnewayduew&^%5d54nc'KH\");";
-                var result = engine.Evaluate(scriptContent);
+
+                //scriptContent += "m(\"SHAURCOnewayduew&^%5d54nc'KH\");";
+                //engine.Execute(scriptContent);  // 方案1：取得脚本里的所有内容，Execute一下，然后，调用engine.Script.func(x,y)执行一下。
+
+                V8Script script = engine.CompileDocument(ScriptFilePath);   // 方案2：载入并编译js文件, 然后Execute, 就可以直接调用。
+                engine.Execute(script);
+                var result = engine.Script.m("SHAURCOnewayduew&^%5d54nc'KH");
 
             }
         }
