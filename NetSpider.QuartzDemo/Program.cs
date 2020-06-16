1fedf2a2206e5c11b56fa3eca9121a3a0f145ecf
Watt Wang <watt.wang@softtek.com>
2020-06-16 18:12:43 +0800

    add QuartzDemo

---
+++ NetSpider.QuartzDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetSpider.QuartzDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
